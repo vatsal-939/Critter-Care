94dc07dcbc21ddde4468d0dbf284152ef0714afd
ashishrathod9 <vatsal.ranpariya04@gmail.com>
2024-10-23 00:48:47 +0530

    updated java file

--- java/com/example/myapplication11/PatientActivity.java
+++ java/com/example/myapplication11/PatientActivity.java
@@ -16,7 +16,6 @@ public class PatientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-
         setContentView(R.layout.activity_patient);
     }
 }
